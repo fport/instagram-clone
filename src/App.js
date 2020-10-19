d363ddca62e680f1192ba8548adf61209cc428a2
Furkan <furkanportakalx@gmail.com>
2020-10-20 00:28:15 +0300

    post component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import './App.css';
-import Post from "./components/Post"
+import React from "react";
+import "./App.css";
+import Post from "./components/Post";
 
 function App() {
   return (
     <div className="app">
-        <div className="app__header">
-            <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram" />
-        </div>
-        <h1>Teknikafa : Let's build an instagram clone!'</h1>
-        <Post/>
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="instagram"
+        />
+      </div>
+      <h1>Teknikafa : Let's build an instagram clone!'</h1>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,28 @@
 import React from "react";
+import "../css/Post.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
-    return(
-        <div>
-            <h3>Username</h3>
-            <img src="https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" alt="" />
-            <h4>Username: caption</h4>
-
-        </div>
-    )
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="PortiCode"
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>Username</h3>
+      </div>
+      <img
+        className="post__image"
+        src="https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+        alt=""
+      />
+      <h4 className="post_text">
+        <strong>porti </strong>
+        wow day three of live sessions
+      </h4>
+    </div>
+  );
 }
 export default Post;
