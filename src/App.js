36c2576b6e02b8c120c62c8985b05533b71b113f
Furkan <furkanportakalx@gmail.com>
2020-10-19 22:46:07 +0300

    basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Post from "./components/Post"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="app__header">
+            <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram" />
+        </div>
+        <h1>Teknikafa : Let's build an instagram clone!'</h1>
+        <Post/>
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Post() {
+    return(
+        <div>
+            <h3>Username</h3>
+            <img src="https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" alt="" />
+            <h4>Username: caption</h4>
+
+        </div>
+    )
+}
+export default Post;
